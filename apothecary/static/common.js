09bde4542f2007610464884496e579648a212af4
admarple <admarple@gmail.com>
2016-10-09 15:22:32 -0400

    Add links over the wedding party graphic

--- apothecary/static/common.js
+++ apothecary/static/common.js
@@ -12,11 +12,40 @@ function validateForm(formName, required) {
     }
 }
 
+function stripSpace(str) {
+    return str.replace(/ /g, '');
+}
+
+/* http://stackoverflow.com/questions/1539367/remove-whitespace-and-line-breaks-between-html-elements-using-jquery */
+jQuery.fn.cleanWhitespace = function() {
+    textNodes = this.contents().filter(
+        function() { return (this.nodeType == 3 && !/\S/.test(this.nodeValue)); })
+        .remove();
+    return this;
+}
+
 $( document ).ready(function() {
-    $('.sections h3').each(function() {
-        if (! this.id || this.id === "") {
-            this.id = this.innerHTML;
+    $('.sections h3').each(function () {
+        if ( ! this.id || this.id === "") {
+            this.id = stripSpace(this.innerHTML);
         }
+    });
+
+    /* display: inline-block doesn't play nicely with whitespace ... */
+    $('#party-links').cleanWhitespace();
+
+    $('.party-figure').click(function() {
+        var target = $('#' + stripSpace($(this).attr('title')));
+        var old_background = jQuery.Color(target, 'background-color');
+        var header = $('.header');
+        $('html, body').animate({
+            scrollTop: target.offset().top - header.height()
+        }, 500);
+        target.css('background-color', jQuery.Color(header, 'background-color'))
+            .delay(1000)
+            .animate({
+                backgroundColor: old_background
+            }, 1000);
     });
 });
 
